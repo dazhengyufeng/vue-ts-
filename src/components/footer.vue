abefb441529059213a01f82c1636dbaf3fa7ff90
郑雨峰 <3057542870@qq.com>
2020-03-26 10:14:05 +0800

    first commit

---
+++ src/components/footer.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <div class="footer"> 技术探索路上的一只菜鸟 </div>
+</template>
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class Footer extends Vue {}
+</script>
+<style scoped>
+.footer {
+  text-align: center;
+  padding: 20px;
+  position: fixed;
+  bottom: 0;
+  font-weight: bold;
+  margin: auto;
+  left: 0;
+  right: 0;
+  /* top: 0; */
+  bottom: 0;
+}
+</style>
+
---
+++ src/views/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app"
+       class="articles-container">
+       <Articles></Articles>
+    <!-- 侧边栏 (暂时去掉，没啥用)-->
+    <Slider></Slider>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Watch } from "vue-property-decorator";
+import Component from "vue-class-component";
+import { Route } from "vue-router";
+import Slider from "@/components/slider.vue"; // @ is an alias to /src
+import Articles from "@/views/articles.vue"; // @ is an alias to /src
+import ArrowUp from "@/components/arrowUp.vue"; // @ is an alias to /src
+import { isMobileOrPc } from "@/utils/utils";
+
+@Component({
+  components: {
+    Slider,
+    Articles
+  }
+})
+export default class App extends Vue {
+}
+</script>
+
+<style lang="less">
+.articles-container{
+    display: flex;
+}
+</style>
